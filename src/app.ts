17b574548f58445975691ecdb9571d6cb6a0da21
Sergey Ovchinnikov <s.ovchinnikov@challenge-insure.ru>
2025-11-16 17:17:47 +0700

    feat: :sparkles: Init

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,6 @@ await migrate.latest();
 await seed.run();
 
 console.log("All migrations and seeds have been run");
+
+// TODO: регулярное получение информации о тарифах wb и сохранение их в БД на каждый день;
+// TODO: регулярное обновление информации о актуальных тарифах в google-таблицах.
